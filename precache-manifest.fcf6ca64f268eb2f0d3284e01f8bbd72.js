b66b02e90c0960dfc143518c1ca3a342ff2693db
sumit <sharmasumit0308@gmail.com>
2020-07-10 07:09:57 -0400

    Updates

---
+++ precache-manifest.fcf6ca64f268eb2f0d3284e01f8bbd72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8c276697b55c7513e9df99a8c6193d2",
+    "url": "/Portfolio_React/index.html"
+  },
+  {
+    "revision": "1465c9b1d04764f5c920",
+    "url": "/Portfolio_React/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/Portfolio_React/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio_React/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1465c9b1d04764f5c920",
+    "url": "/Portfolio_React/static/js/main.feff0a29.chunk.js"
+  },
+  {
+    "revision": "ba16ad250fdc4cd3b032",
+    "url": "/Portfolio_React/static/js/runtime-main.574eb423.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portfolio_React/static/media/logo.5d5d9eef.svg"
+  }
+]);
